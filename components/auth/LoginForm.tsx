bbf6531d5ec3ba6f90af7197bd297ae22adcf245
rsonetv <rafalxxu@gmail.com>
2025-08-02 03:31:45 +0200

    Poprawki wizualne w formularzach autoryzacji

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -57,6 +57,17 @@ export default function LoginForm() {
         </LoadingButton>
       </form>
 
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Lub kontynuuj za pomocą
+          </span>
+        </div>
+      </div>
+
       <OAuthButtons />
 
       <div className="flex justify-between text-sm">
--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -78,6 +78,17 @@ export default function RegisterForm() {
         </LoadingButton>
       </form>
 
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Lub kontynuuj za pomocą
+          </span>
+        </div>
+      </div>
+
       <OAuthButtons />
 
       <div className="text-center text-sm">
